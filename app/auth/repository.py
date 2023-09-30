c942244c0fa0c48331010f3daf193fd68fd1b52a
Vikash Kumar <vikas.kumar@mitratech.com>
2023-09-30 22:39:20 +0530

    Added repository file and remove bussiness logic from router file to repository file

---
+++ app/auth/repository.py
@@ -0,0 +1,113 @@
+from fastapi import status, HTTPException
+from sqlalchemy.orm import Session
+
+from app.auth.model import User
+from app.auth.schema import (
+    ChangePasswordSchema,
+    ResetPasswordSchema,
+    SendTokenSchema,
+    SigninSchema,
+    SignupSchema,
+    VerifyEmailSchema
+)
+from app.utils.auth import (
+    create_verification_token,
+    verify_verification_token
+)
+from app.utils.hashing import HashPassword
+
+
+def signup_repository(request: SignupSchema, db: Session):
+    if db.query(User).filter(User.email == request.email).first():
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, detail="Email already in used")
+    if db.query(User).filter(User.username == request.username).first():
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, detail="Username already in used")
+    password_context = HashPassword()
+    hashed_password = password_context.get_hash_password(request.password)
+    user = User(
+        first_name=request.first_name,
+        last_name=request.last_name,
+        username=request.username,
+        email=request.email,
+        password=hashed_password,
+    )
+    db.add(user)
+    db.commit()
+    token = create_verification_token(user.username)
+    return token
+
+
+def signin_repository(request: SigninSchema, db: Session):
+    user = db.query(User).filter(User.email == request.email).first()
+    if user is None:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, detail="Wrong credentials")
+    if not user.verified:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, detail="Please verify your email")
+    if not user.active:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, detail="User is not active")
+    password_context = HashPassword()
+    if not password_context.verify_hash_password(request.password, user.password):
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, detail="Wrong credentials")
+    return user
+
+
+def change_password_repository(request: ChangePasswordSchema, db: Session, user: User):
+    password_context = HashPassword()
+    auth_user = db.query(User).filter(User.id == user.id).first()
+    if request.password1 != request.password2:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                            detail="Password1 and password2 must be same")
+    if not password_context.verify_hash_password(request.current_password, auth_user.password):
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, detail="Password mis-match")
+    if password_context.verify_hash_password(request.password1, auth_user.password):
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                            detail="Password same as current password")
+    auth_user.password = password_context.get_hash_password(request.password1)
+    db.add(auth_user)
+    db.commit()
+
+
+def verify_email_repository(request: VerifyEmailSchema, db: Session):
+    user = verify_verification_token(request.token)
+    auth_user = db.query(User).filter(User.id == user.id).first()
+    auth_user.verified = True
+    auth_user.active = True
+    db.add(auth_user)
+    db.commit()
+
+
+def resend_verify_email_repository(request: SendTokenSchema, db: Session):
+    user = db.query(User).filter(User.email == request.email).first()
+    if user is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+    token = create_verification_token(user.username)
+    return token
+
+
+def forgot_password_repository(request: SendTokenSchema, db: Session):
+    user = db.query(User).filter(User.email == request.email).first()
+    if user is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+    token = create_verification_token(user.username, token_type="reset")
+    return token
+
+
+def reset_password_repository(request: ResetPasswordSchema, db: Session):
+    if request.password1 != request.password2:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, detail="Password mis-match")
+    user = verify_verification_token(request.token, token_type="reset")
+    auth_user = db.query(User).filter(User.id == user.id).first()
+    password_context = HashPassword()
+    auth_user.password = password_context.get_hash_password(request.password1)
+    db.add(auth_user)
+    db.commit()
--- app/auth/router.py
+++ app/auth/router.py
@@ -1,7 +1,16 @@
-from fastapi import APIRouter, Depends, status, HTTPException
+from fastapi import APIRouter, Depends, status
 from sqlalchemy.orm import Session
 
 from app.auth.model import User
+from app.auth.repository import (
+    change_password_repository,
+    forgot_password_repository,
+    resend_verify_email_repository,
+    reset_password_repository,
+    signin_repository,
+    signup_repository,
+    verify_email_repository
+)
 from app.auth.schema import (
     AuthUserSchema,
     ChangePasswordSchema,
@@ -17,12 +26,9 @@ from app.database import get_db
 from app.utils.auth import (
     create_access_token,
     create_refresh_token,
-    create_verification_token,
     get_current_user,
     validate_refresh_token,
-    verify_verification_token
 )
-from app.utils.hashing import HashPassword
 
 
 router = APIRouter(
@@ -33,43 +39,13 @@ router = APIRouter(
 
 @router.post("/signup", status_code=status.HTTP_200_OK)
 def signup(request: SignupSchema, db: Session = Depends(get_db)):
-    if db.query(User).filter(User.email == request.email).first():
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST, detail="Email already in used")
-    if db.query(User).filter(User.username == request.username).first():
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST, detail="Username already in used")
-    password_context = HashPassword()
-    hashed_password = password_context.get_hash_password(request.password)
-    user = User(
-        first_name=request.first_name,
-        last_name=request.last_name,
-        username=request.username,
-        email=request.email,
-        password=hashed_password,
-    )
-    db.add(user)
-    db.commit()
-    token = create_verification_token(user.username)
+    token = signup_repository(request, db)
     return {"detail": "Signup successfully!", "token": token}
 
 
 @router.post("/signin", status_code=status.HTTP_200_OK, response_model=TokenSchema)
 def signin(request: SigninSchema, db: Session = Depends(get_db)):
-    user = db.query(User).filter(User.email == request.email).first()
-    if user is None:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST, detail="Wrong credentials")
-    if not user.verified:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST, detail="Please verify your email")
-    if not user.active:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST, detail="User is not active")
-    password_context = HashPassword()
-    if not password_context.verify_hash_password(request.password, user.password):
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST, detail="Wrong credentials")
+    user = signin_repository(request, db)
     return {
         "access_token": create_access_token(user.username),
         "refresh_token": create_refresh_token(user.username),
@@ -92,63 +68,29 @@ def refresh_token(request: RefreshTokenSchema, db: Session = Depends(get_db)):
 
 @router.post("/change-password", status_code=status.HTTP_200_OK)
 def change_password(request: ChangePasswordSchema, db: Session = Depends(get_db), user: User = Depends(get_current_user)):
-    password_context = HashPassword()
-    auth_user = db.query(User).filter(User.id == user.id).first()
-    if request.password1 != request.password2:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
-                            detail="Password1 and password2 must be same")
-    if not password_context.verify_hash_password(request.current_password, auth_user.password):
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST, detail="Password mis-match")
-    if password_context.verify_hash_password(request.password1, auth_user.password):
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
-                            detail="Password same as current password")
-    auth_user.password = password_context.get_hash_password(request.password1)
-    db.add(auth_user)
-    db.commit()
+    change_password_repository(request, db, user)
     return {"detail": "Change password successfully!"}
 
 
 @router.post("/email-verify", status_code=status.HTTP_200_OK)
 def email_verify(request: VerifyEmailSchema, db: Session = Depends(get_db)):
-    user = verify_verification_token(request.token)
-    auth_user = db.query(User).filter(User.id == user.id).first()
-    auth_user.verified = True
-    auth_user.active = True
-    db.add(auth_user)
-    db.commit()
+    verify_email_repository(request, db)
     return {"detail": "Email verified successfully!"}
 
 
 @router.post("/resend-email", status_code=status.HTTP_200_OK)
 def resend_email(request: SendTokenSchema, db: Session = Depends(get_db)):
-    user = db.query(User).filter(User.email == request.email).first()
-    if user is None:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
-    token = create_verification_token(user.username)
+    token = resend_verify_email_repository(request, db)
     return {"detail": token}
 
 
 @router.post("/forgot-password", status_code=status.HTTP_200_OK)
 def forgot_password(request: SendTokenSchema, db: Session = Depends(get_db)):
-    user = db.query(User).filter(User.email == request.email).first()
-    if user is None:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
-    token = create_verification_token(user.username, token_type="reset")
+    token = forgot_password_repository(request, db)
     return {"detail": token}
 
 
 @router.post("/reset-password", status_code=status.HTTP_200_OK)
 def reset_password(request: ResetPasswordSchema, db: Session = Depends(get_db)):
-    if request.password1 != request.password2:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST, detail="Password mis-match")
-    user = verify_verification_token(request.token, token_type="reset")
-    auth_user = db.query(User).filter(User.id == user.id).first()
-    password_context = HashPassword()
-    auth_user.password = password_context.get_hash_password(request.password1)
-    db.add(auth_user)
-    db.commit()
+    reset_password_repository(request, db)
     return {"detail": "Reset password successfully!"}
