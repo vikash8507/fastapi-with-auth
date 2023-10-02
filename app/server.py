7395efc9b6e6f39993eed763749dff42321f1816
Vikash Kumar <vikas.kumar@mitratech.com>
2023-10-02 22:48:26 +0530

    Some server and swagger configurations

--- app/server.py
+++ app/server.py
@@ -3,12 +3,26 @@ from fastapi.responses import FileResponse
 
 from app.blog.router import router as blog_router
 from app.auth.router import router as auth_router
-from app.blog import model
+from app.blog import model as blog_model
+from app.auth import model as auth_model
 from app.database import engine
 
-model.Base.metadata.create_all(bind=engine)
+blog_model.Base.metadata.create_all(bind=engine)
+auth_model.Base.metadata.create_all(bind=engine)
 
-app = FastAPI()
+app = FastAPI(
+    docs_url="/api/docs",
+    redoc_url="/api/redocs",
+    description="Api using FastAPI",
+    servers=[
+        {"url": "dev.api.fastapi.com"},
+        {"url": "qa.api.fastapi.com"},
+        {"url": "stage.api.fastapi.com"},
+        {"url": "sandbox.api.fastapi.com"},
+        {"url": "runtime.api.fastapi.com"},
+        {"url": "api.fastapi.com"},
+    ],
+)
 
 app.include_router(auth_router)
 app.include_router(blog_router)
