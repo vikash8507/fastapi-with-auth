60cb142bb74f1d50ec337140bb51cb005bdb59b0
Vikash Kumar <vikas.kumar@mitratech.com>
2023-10-02 21:39:16 +0530

    Added api for show uploaded file

--- app/server.py
+++ app/server.py
@@ -1,5 +1,5 @@
-from typing import Union
-from fastapi import FastAPI
+from fastapi import FastAPI, status, HTTPException
+from fastapi.responses import FileResponse
 
 from app.blog.router import router as blog_router
 from app.auth.router import router as auth_router
@@ -16,3 +16,10 @@ app.include_router(blog_router)
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+@app.get("/{directory}/{file}", status_code=status.HTTP_200_OK)
+def show_uploaded_file(directory: str, file: str):
+    try:
+        return  FileResponse(f"{directory}/{file}")
+    except:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Not found")
